eaa4950efeac043f6d69d534f99efa37460baaf3
Mike Burgess <mjburgessjr@gmail.com>
2023-11-08 15:41:00 -0500

    Check-in example data

--- grasp_and_record.py
+++ grasp_and_record.py
@@ -1,29 +1,45 @@
 import time
 
-from frankapy import FrankaArm
+# from frankapy import FrankaArm
+# from wedge_video import GelsightWedgeVideo
+# from contact_force import ContactForce
+# from data_recorder import DataRecorder
+
+# fa = FrankaArm()
+
+# # Define streaming addresses
+# wedge_video     =   GelsightWedgeVideo(IP="10.10.10.100", config_csv="./config.csv")
+# contact_force   =   ContactForce(IP="10.10.10.50", port=8888)
+# data_recorder   =   DataRecorder(wedge_video=wedge_video, contact_force=contact_force)
+
+# # Start recording
+# data_recorder.start_stream(plot=True, plot_diff=True, plot_depth=True)
+# time.sleep(1)
+
+# # Close gripper
+# # fa.close_gripper()
+# fa.goto_gripper(0.08)
+# time.sleep(1)
+# fa.goto_gripper(0.00)
+# time.sleep(1)
+# fa.goto_gripper(0.08)
+
+# # Stop recording and save
+# data_recorder.end_stream()
+# data_recorder.save('./data/grasp_foam_brick')
+
+import os
 from wedge_video import GelsightWedgeVideo
 from contact_force import ContactForce
 from data_recorder import DataRecorder
 
-fa = FrankaArm()
 
-# Define streaming addresses
-wedge_video     =   GelsightWedgeVideo(IP="10.10.10.100", config_csv="./config.csv")
-contact_force   =   ContactForce(IP="10.10.10.50", port=8888)
-data_recorder   =   DataRecorder(wedge_video=wedge_video, contact_force=contact_force)
-
-# Start recording
-data_recorder.start_stream(plot=True, plot_diff=True, plot_depth=True)
-time.sleep(1)
-
-# Close gripper
-# fa.close_gripper()
-fa.goto_gripper(0.08)
-time.sleep(1)
-fa.goto_gripper(0.02)
-time.sleep(1)
-fa.goto_gripper(0.08)
-
-# Stop recording and save
-data_recorder.end_stream()
-# data_recorder.save('./grasp_small_sphere')
+for filename in os.listdir("./example_data"):
+    if filename.endswith('.pkl'):
+        cf = ContactForce()
+        cf.load('./example_data/' + filename)
+        maxf = 0
+        for f in cf.forces():
+            if f != None:
+                if abs(f) >= abs(maxf): maxf = f
+        print(filename, maxf)
