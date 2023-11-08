9e7b145733f607fd48b51cf9d5a285e57b37f62d
Mike Burgess <mjburgessjr@gmail.com>
2023-11-08 17:10:35 -0500

    More data

--- grasp_and_record.py
+++ grasp_and_record.py
@@ -12,21 +12,22 @@ wedge_video     =   GelsightWedgeVideo(IP="10.10.10.100", config_csv="./config.c
 contact_force   =   ContactForce(IP="10.10.10.50", port=8888)
 data_recorder   =   DataRecorder(wedge_video=wedge_video, contact_force=contact_force)
 
+fa.goto_gripper(0.08)
+
 # Start recording
 data_recorder.start_stream(plot=True, plot_diff=True, plot_depth=True)
-time.sleep(1)
 
 # Close gripper
 # fa.close_gripper()
 fa.goto_gripper(0.08)
-time.sleep(2)
-fa.goto_gripper(0.00)
-time.sleep(3)
+time.sleep(0.5)
+fa.goto_gripper(0.02)
+time.sleep(0.5)
 fa.goto_gripper(0.08)
 
-OBJECT_NAME = ''
+OBJECT_NAME = 'foam_earth'
 
 # Stop recording and save
 data_recorder.auto_clip()
 data_recorder.end_stream()
-data_recorder.save(f'./data/{OBJECT_NAME}')
+data_recorder.save(f'./example_data/{OBJECT_NAME}')
