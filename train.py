ae3303d4081aed21ffc8b273de68b7bc1d344e85
Mike Burgess <mjburgessjr@gmail.com>
2024-02-06 21:56:23 -0500

    Switch LR back

--- train.py
+++ train.py
@@ -755,7 +755,7 @@ if __name__ == "__main__":
         'batch_size'     : 32,
         'feature_size'   : 512,
         'val_pct'        : 0.2,
-        'learning_rate'  : 1e-6,
+        'learning_rate'  : 1e-5,
         'gamma'          : 0.5,
         'lr_step_size'   : 30,
         'random_state'   : 40,
