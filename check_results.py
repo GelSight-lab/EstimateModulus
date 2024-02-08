d4a83bc287e480f3878bbb579c281a4fd643cd5f
Mike Burgess <mjburgessjr@gmail.com>
2024-02-08 11:04:26 -0500

    Check results

---
+++ check_results.py
@@ -0,0 +1,32 @@
+
+import json
+
+run_names = [
+    'LR=1e-4_t=0', 'LR=1e-4_t=1', 'LR=1e-5_t=0', 'LR=1e-5_t=1'
+]
+
+with open(f'./model/{run_names[0]}/poorly_predicted.json', 'r') as file:
+    run0_dict = json.load(file)
+poorly_predicted_in_all = {}
+for key in run0_dict.keys():
+    poorly_predicted_in_all[key] = True
+
+for run_name in run_names:
+    with open(f'./model/{run_name}/poorly_predicted.json', 'r') as file:
+        pp = json.load(file)
+
+    for obj in pp.keys():
+        if pp[obj] == False:
+            poorly_predicted_in_all[key] = False
+    
+    with open(f'./model/{run_name}/material_log_acc.json', 'r') as file:
+        mat = json.load(file)
+    with open(f'./model/{run_name}/shape_log_acc.json', 'r') as file:
+        shap = json.load(file)
+
+    print(run_name)
+    print(mat)
+    print(shap)
+    print('\n')
+
+print('Poorly predicted in all:', poorly_predicted_in_all, '\n')
