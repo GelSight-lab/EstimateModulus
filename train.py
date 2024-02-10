e97b41d26feedcd213f1f1a22a9458f2ba1bfa8e
Mike Burgess <mjburgessjr@gmail.com>
2024-02-10 17:54:29 -0500

    Try again

--- train.py
+++ train.py
@@ -463,10 +463,13 @@ class ModulusModel():
             abs_log_diff = torch.abs(torch.log10(self.log_unnormalize(outputs)) - torch.log10(self.log_unnormalize(y)))
             train_avg_log_diff += abs_log_diff.sum().item()
             for i in range(self.batch_size):
-                self.train_object_performance[object_names[i]].append(abs_log_diff[i])
+                self.train_object_performance[object_names[i]]['total_log_diff'] += abs_log_diff[i]
+                self.train_object_performance[object_names[i]]['count'] += 1
                 if abs_log_diff[i] <= 0.5:
+                    self.train_object_performance[object_names[i]]['total_log_acc'] += 1
                     train_log_acc += 1
                 if abs_log_diff[i] >= 2:
+                    self.train_object_performance[object_names[i]]['total_poorly_predicted'] += 1
                     train_pct_with_100_factor_err += 1
                     
 
@@ -520,10 +523,13 @@ class ModulusModel():
             abs_log_diff = torch.abs(torch.log10(self.log_unnormalize(outputs)) - torch.log10(self.log_unnormalize(y)))
             val_avg_log_diff += abs_log_diff.sum().item()
             for i in range(self.batch_size):
-                self.val_object_performance[object_names[i]].append(abs_log_diff[i])
+                self.val_object_performance[object_names[i]]['total_log_diff'] += abs_log_diff[i]
+                self.val_object_performance[object_names[i]]['count'] += 1
                 if abs_log_diff[i] <= 0.5:
+                    self.val_object_performance[object_names[i]]['total_log_acc'] += 1
                     val_log_acc += 1
                 if abs_log_diff[i] >= 2:
+                    self.val_object_performance[object_names[i]]['total_poorly_predicted'] += 1
                     val_pct_with_100_factor_err += 1
         
         # Return loss and accuracy
