213c8959a78ed496d478e35b8620bef4c7e93d31
Mike Burgess <mjburgessjr@gmail.com>
2024-02-12 21:26:38 -0500

    Logging

--- nn_modules.py
+++ nn_modules.py
@@ -236,11 +236,11 @@ class EstimationFC(nn.Module):
         self.drop = nn.Dropout(self.dropout_pct)
 
     def forward(self, x):
-        print(x.device, x[0])
+        print(x.device, x.shape, x[0])
         x = self.fc1(x)
-        print(x.device, x[0])
+        print(x.device, x.shape, x[0])
         x = F.silu(x)
-        print(x.device, x[0])
+        print(x.device, x.shape, x[0])
         x = self.drop(x)
         print(x.device, x[0])
         x = self.fc2(x)
