b0b40cb68c86b367f5aafba26bb99f247262b942
Mike Burgess <mjburgessjr@gmail.com>
2024-02-18 23:22:06 -0500

    Take out 1e-2

--- train.py
+++ train.py
@@ -930,10 +930,10 @@ if __name__ == "__main__":
 
     base_run_name = config['run_name']
     LR_to_epoch_dropout = {
-        '1e-2': [40, 0.3],
         '1e-3': [50, 0.3],
         '1e-3': [50, 0.5],
         '1e-5': [120, 0.3],
+        '1e-4': [70, 0.3],
     }
     for lr in LR_to_epoch_dropout.keys():
         config['learning_rate'] = float(lr)
