3e456daa67f14b4c55a60ffb24d367d4e196edf1
Mike Burgess <mjburgessjr@gmail.com>
2024-02-20 21:23:44 -0500

    Fix scheduler

--- train.py
+++ train.py
@@ -972,7 +972,7 @@ if __name__ == "__main__":
         'val_pct'           : 0.175,
         'dropout_pct'       : 0.4,
         'learning_rate'     : 2.5e-4,
-        'gamma'             : 100**(-5/50), # 100**(-lr_step_size / epochs)
+        'gamma'             : 100**(-5/150), # 100**(-lr_step_size / epochs)
         'lr_step_size'      : 5,
         'random_state'      : 95,
     }
