51f7ef5611c1380427472cc296647a5f8d879883
Mike Burgess <mjburgessjr@gmail.com>
2024-02-21 19:12:27 -0500

    Naming

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.cuda.empty_cache()
 torch.autograd.set_detect_anomaly(True)
 
-DATA_DIR = './data' # '/media/mike/Elements/data'
+DATA_DIR = '/media/mike/Elements/data'
 N_FRAMES = 3
 WARPED_CROPPED_IMG_SIZE = (250, 350) # WARPED_CROPPED_IMG_SIZE[::-1]
 
@@ -967,7 +967,7 @@ if __name__ == "__main__":
 
         # Logging on/off
         'use_wandb': True,
-        'run_name': 'Classification__CustomCNN',
+        'run_name': 'Regression__CustomCNN',
 
         # Training and model parameters
         'epochs'            : 150,
