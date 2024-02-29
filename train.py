6e17d57cf3439f3af3a403cb996531bc4648d085
Mike Burgess <mjburgessjr@gmail.com>
2024-02-29 13:03:00 -0500

    Try cropping again

--- train.py
+++ train.py
@@ -276,7 +276,8 @@ class ModulusModel():
             self.random_transformer = torchvision.transforms.Compose([
                     torchvision.transforms.RandomHorizontalFlip(0.25),
                     torchvision.transforms.RandomVerticalFlip(0.25),
-                    torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.0, hue=0.0),
+                    torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.0, hue=0.05),
+                    torchvision.transforms.RandomResizedCrop(size=(self.img_size[0], self.img_size[1]), scale=(0.95, 1.0), antialias=True),
                     # torchvision.transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.0001, 1.5)),
                 ])
 
@@ -975,7 +976,7 @@ if __name__ == "__main__":
 
         # Logging on/off
         'use_wandb': True,
-        'run_name': 'Batch96_ContrastAndBrightness_NoNormalization',
+        'run_name': 'Batch96_CropBrightnessHueContrast_NoNormalization',
 
         # Training and model parameters
         'epochs'            : 80,
