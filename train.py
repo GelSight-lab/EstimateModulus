8bd6c771d82f72db0494e8f40dd374f2db894a50
Mike Burgess <mjburgessjr@gmail.com>
2024-02-29 11:27:03 -0500

    Try contrast

--- train.py
+++ train.py
@@ -276,7 +276,7 @@ class ModulusModel():
             self.random_transformer = torchvision.transforms.Compose([
                     torchvision.transforms.RandomHorizontalFlip(0.25),
                     torchvision.transforms.RandomVerticalFlip(0.25),
-                    torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.0, saturation=0.0, hue=0.1),
+                    torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.0, hue=0.0),
                     # torchvision.transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.0001, 1.5)),
                 ])
 
@@ -975,7 +975,7 @@ if __name__ == "__main__":
 
         # Logging on/off
         'use_wandb': True,
-        'run_name': 'Batch96_HueAndBrightness_NoNormalization',
+        'run_name': 'Batch96_ContrastAndBrightness_NoNormalization',
 
         # Training and model parameters
         'epochs'            : 80,
