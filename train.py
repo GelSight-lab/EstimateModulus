d8399930a5a9bf062fd95c641b179ae3fcda665c
Mike Burgess <mjburgessjr@gmail.com>
2024-02-29 15:06:31 -0500

    Just brightness and contrast again

--- train.py
+++ train.py
@@ -277,7 +277,7 @@ class ModulusModel():
                     torchvision.transforms.RandomHorizontalFlip(0.25),
                     torchvision.transforms.RandomVerticalFlip(0.25),
                     torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.0, hue=0.0),
-                    torchvision.transforms.RandomResizedCrop(size=(self.img_size[0], self.img_size[1]), scale=(0.95, 1.0), antialias=True),
+                    # torchvision.transforms.RandomResizedCrop(size=(self.img_size[0], self.img_size[1]), scale=(0.95, 1.0), antialias=True),
                     # torchvision.transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.0001, 1.5)),
                 ])
 
@@ -976,7 +976,7 @@ if __name__ == "__main__":
 
         # Logging on/off
         'use_wandb': True,
-        'run_name': 'Batch96_CropBrightnessContrast_NoNormalization',
+        'run_name': 'Batch96_BrightnessContrast_NoNormalization',
 
         # Training and model parameters
         'epochs'            : 120,
