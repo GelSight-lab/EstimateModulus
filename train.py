85bc5884bc7f9d1ed4e3212abb7e568fc470fa0a
Mike Burgess <mjburgessjr@gmail.com>
2024-03-04 11:04:36 -0500

    More epochs

--- train.py
+++ train.py
@@ -1048,7 +1048,7 @@ if __name__ == "__main__":
         'run_name': 'Batch32_Estimations',
 
         # Training and model parameters
-        'epochs'            : 60,
+        'epochs'            : 100,
         'batch_size'        : 32,
         'pretrained_CNN'    : False,
         'use_RNN'           : False, # True,
