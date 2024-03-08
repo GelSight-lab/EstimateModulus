0c3543da7e5e3314a318f236b14804cc28c824d1
Mike Burgess <mjburgessjr@gmail.com>
2024-03-08 12:58:38 -0500

    Change entry dropouts back

--- train.py
+++ train.py
@@ -1079,7 +1079,7 @@ if __name__ == "__main__":
 
         # Logging on/off
         'use_wandb': True,
-        'run_name': 'EstDecoderEntryDropout_Normalized_ExcludeTo200',
+        'run_name': 'ClassicDropout_Normalized_ExcludeTo200',
 
         # Training and model parameters
         'epochs'            : 60,
