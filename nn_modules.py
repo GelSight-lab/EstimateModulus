879fb9f6e5e32cd106fb56ead54e57ae8eb5c15c
Mike Burgess <mjburgessjr@gmail.com>
2024-03-16 14:16:27 -0400

    Try maxpool

--- nn_modules.py
+++ nn_modules.py
@@ -22,8 +22,7 @@ class EncoderCNN(nn.Module):
                  img_x=200,
                  img_y=200,
                  input_channels=1,
-                 fc_hidden1=1024, # 256,
-                 fc_hidden2=512, # 64,
+                 fc_hidden=512,
                  dropout_pct=0.5,
                  CNN_embed_dim=128):
         super(EncoderCNN, self).__init__()
@@ -33,10 +32,9 @@ class EncoderCNN(nn.Module):
         self.CNN_embed_dim = CNN_embed_dim
 
         # Fully connected layer hidden nodes
-        self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
+        self.fc_hidden = fc_hidden
         self.dropout_pct = dropout_pct
 
-        '''
         # CNN architechtures
         self.ch1, self.ch2, self.ch3, self.ch4, self.ch5 = 32, 64, 128, 256, 512
         self.k1, self.k2, self.k3, self.k4, self.k5 = (5, 5), (3, 3), (3, 3), (3, 3), (3, 3)  # 2d kernal size
@@ -106,100 +104,25 @@ class EncoderCNN(nn.Module):
         )
 
         self.drop = nn.Dropout(self.dropout_pct)
-        # self.pool = nn.MaxPool2d(2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-        # self.fc1 = nn.Linear(
-        #     self.ch5, # self.ch5 * self.conv5_outshape[0] * self.conv5_outshape[1],
-        #     self.CNN_embed_dim
-        # ) # Fully connected layer, output k classes
+        self.pool = nn.MaxPool2d(2)
+        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
         self.fc1 = nn.Linear(
-            self.ch5, # self.ch5 * self.conv5_outshape[0] * self.conv5_outshape[1],
-            self.fc_hidden1
+            self.ch5 * (self.conv5_outshape[0] // 2) * (self.conv5_outshape[1] // 2), # self.ch5 * self.conv5_outshape[0] * self.conv5_outshape[1],
+            self.fc_hidden
         ) # Fully connected layer, output k classes
         self.fc2 = nn.Linear(
-            self.fc_hidden1,
+            self.fc_hidden,
             self.CNN_embed_dim
         )  # Output = CNN embedding latent variables
-        '''
-
-        
-        # CNN architechtures
-        self.ch1, self.ch2, self.ch3, self.ch4 = 8, 16, 32, 64
-        self.k1, self.k2, self.k3, self.k4 = (5, 5), (3, 3), (3, 3), (
-            3, 3)  # 2d kernal size
-        self.s1, self.s2, self.s3, self.s4 = (2, 2), (2, 2), (2, 2), (
-            2, 2)  # 2d strides
-        self.pd1, self.pd2, self.pd3, self.pd4 = (0, 0), (0, 0), (0, 0), (
-            0, 0)  # 2d padding
-
-        # conv2D output shapes
-        self.conv1_outshape = conv2D_output_size((self.img_x, self.img_y),
-                                                 self.pd1, self.k1,
-                                                 self.s1)  # Conv1 output shape
-        self.conv2_outshape = conv2D_output_size(self.conv1_outshape, self.pd2,
-                                                 self.k2, self.s2)
-        self.conv3_outshape = conv2D_output_size(self.conv2_outshape, self.pd3,
-                                                 self.k3, self.s3)
-        self.conv4_outshape = conv2D_output_size(self.conv3_outshape, self.pd4,
-                                                 self.k4, self.s4)
-        
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_channels,
-                      out_channels=self.ch1,
-                      kernel_size=self.k1,
-                      stride=self.s1,
-                      padding=self.pd1),
-            nn.BatchNorm2d(self.ch1, momentum=0.01),
-            nn.ReLU(inplace=True),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=self.ch1,
-                      out_channels=self.ch2,
-                      kernel_size=self.k2,
-                      stride=self.s2,
-                      padding=self.pd2),
-            nn.BatchNorm2d(self.ch2, momentum=0.01),
-            nn.ReLU(inplace=True),
-        )
-
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=self.ch2,
-                      out_channels=self.ch3,
-                      kernel_size=self.k3,
-                      stride=self.s3,
-                      padding=self.pd3),
-            nn.BatchNorm2d(self.ch3, momentum=0.01),
-            nn.ReLU(inplace=True),
-        )
-
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=self.ch3,
-                      out_channels=self.ch4,
-                      kernel_size=self.k4,
-                      stride=self.s4,
-                      padding=self.pd4),
-            nn.BatchNorm2d(self.ch4, momentum=0.01),
-            nn.ReLU(inplace=True),
-        )
-
-        self.drop = nn.Dropout(self.dropout_pct)
-        self.pool = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(
-            self.ch4 * self.conv4_outshape[0] * self.conv4_outshape[1],
-            self.fc_hidden1)
-        self.fc2 = nn.Linear(
-            self.fc_hidden1,
-            self.CNN_embed_dim)  # output = CNN embedding latent variables
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
-        # x = self.conv5(x)
-        # x = self.avgpool(x)
+        x = self.conv5(x)
+        x = self.pool(x)
         x = x.view(x.size(0), -1) # Flatten the output of conv
         x = self.drop(x)
         x = self.fc1(x)
@@ -241,7 +164,7 @@ class ModifiedResNet18(nn.Module):
 class DecoderFC(nn.Module):
     def __init__(self,
                 input_dim=N_FRAMES * 512,
-                FC_layer_nodes=[512, 512, 256, 64], # [512, 256, 128, 32],
+                FC_layer_nodes=[512, 256, 128, 64], # [512, 256, 128, 32],
                 dropout_pct=0.5,
                 output_dim=1):
         super(DecoderFC, self).__init__()
