6d57b8d02fd918b879e4672e61b2182f89723b8f
Mike Burgess <mjburgessjr@gmail.com>
2024-03-16 21:01:32 -0400

    Sigmoid again

--- train.py
+++ train.py
@@ -1013,7 +1013,7 @@ if __name__ == "__main__":
 
         # Logging on/off
         'use_wandb': True,
-        'run_name': 'SiLuActivation_OutputBy10_MikeyCNN_Decoder4Layer_Normalized_ExcludeTo200',
+        'run_name': 'OutputBy10_MikeyCNN_Decoder4Layer_Normalized_ExcludeTo200',
 
         # Training and model parameters
         'epochs'            : 200,
@@ -1024,7 +1024,7 @@ if __name__ == "__main__":
         'val_pct'           : 0.175,
         'dropout_pct'       : 0.4,
         'random_mask_pct'   : 0.1,
-        'learning_rate'     : 5e-6, # 1e-5, # (for estimations), # 1e-5 (for no estimations)
+        'learning_rate'     : 3e-6, # 1e-5, # (for estimations), # 1e-5 (for no estimations)
         'gamma'             : 1, # 100**(-5/150), # 100**(-lr_step_size / epochs)
         'lr_step_size'      : 5,
         'random_state'      : 27,
