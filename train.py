62805bbcb2a1ba8562e8f0f642299b8b79fe8a4e
Mike Burgess <mjburgessjr@gmail.com>
2024-03-18 23:52:26 -0400

    Lower LR

--- train.py
+++ train.py
@@ -1052,7 +1052,7 @@ if __name__ == "__main__":
         'fwe_feature_size'  : 32,
         'val_pct'           : 0.175,
         'dropout_pct'       : 0.3,
-        'learning_rate'     : 5e-6, # 1e-5
+        'learning_rate'     : 3e-6, # 1e-5
         'gamma'             : 1, # 100**(-5/150), # 100**(-lr_step_size / epochs)
         'lr_step_size'      : 5,
         'random_state'      : 27,
