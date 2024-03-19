0b9f2df20939383b0311409ffead6ec754b805ee
Mike Burgess <mjburgessjr@gmail.com>
2024-03-18 23:18:07 -0400

    Remove dropout fully

--- train.py
+++ train.py
@@ -1042,7 +1042,7 @@ if __name__ == "__main__":
 
         # Logging on/off
         'use_wandb': True,
-        'run_name': 'EstDropoutOff_EstBiased_FWLayer1_ExcludeTo200',
+        'run_name': 'NoDropout_EstBiased_FWLayer1_ExcludeTo200',
 
         # Training and model parameters
         'epochs'            : 80,
@@ -1051,7 +1051,7 @@ if __name__ == "__main__":
         'img_feature_size'  : 128,
         'fwe_feature_size'  : 32,
         'val_pct'           : 0.175,
-        'dropout_pct'       : 0.4,
+        'dropout_pct'       : 0.0, # 0.5
         'learning_rate'     : 1e-5, # 1e-5
         'gamma'             : 1, # 100**(-5/150), # 100**(-lr_step_size / epochs)
         'lr_step_size'      : 5,
