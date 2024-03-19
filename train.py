550d98308978047660bb64c75aa25359b54d4610
Mike Burgess <mjburgessjr@gmail.com>
2024-03-19 17:25:52 -0400

    Slower LR

--- train.py
+++ train.py
@@ -1059,7 +1059,7 @@ if __name__ == "__main__":
         'fwe_feature_size'  : 32,
         'val_pct'           : 0.175,
         'dropout_pct'       : 0.3,
-        'learning_rate'     : 5e-6, # 1e-5,
+        'learning_rate'     : 3e-6, # 5e-6, # 1e-5,
         'gamma'             : 0.975, # 100**(-5/150), # 100**(-lr_step_size / epochs)
         'lr_step_size'      : 1,
         'random_state'      : 27,
