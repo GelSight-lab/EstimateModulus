dec8fe41e3a634f3e0b2fc58db3eba8c66738f26
Mike Burgess <mjburgessjr@gmail.com>
2024-03-19 22:39:06 -0400

    And arch change

--- nn_modules.py
+++ nn_modules.py
@@ -255,7 +255,7 @@ class DecoderFC(nn.Module):
 class EstimationDecoderFC(nn.Module):
     def __init__(self,
                 input_dim=6,
-                FC_layer_nodes=[64, 64, 32],
+                FC_layer_nodes=[64, 32],
                 dropout_pct=0.0,
                 output_dim=1):
         super(EstimationDecoderFC, self).__init__()
@@ -265,12 +265,12 @@ class EstimationDecoderFC(nn.Module):
         self.dropout_pct = dropout_pct
         self.output_dim = output_dim
 
-        assert len(FC_layer_nodes) == 3
+        assert len(FC_layer_nodes) == 2
 
         self.fc1 = nn.Linear(self.FC_input_size, self.FC_layer_nodes[0])
         self.fc2 = nn.Linear(self.FC_layer_nodes[0], self.FC_layer_nodes[1])
-        self.fc3 = nn.Linear(self.FC_layer_nodes[1], self.FC_layer_nodes[2])
-        self.fc4 = nn.Linear(self.FC_layer_nodes[2], self.output_dim)
+        self.fc3 = nn.Linear(self.FC_layer_nodes[1], self.output_dim) # self.FC_layer_nodes[2])
+        # self.fc4 = nn.Linear(self.FC_layer_nodes[2], self.output_dim)
         self.drop = nn.Dropout(self.dropout_pct)
 
     def forward(self, x):
@@ -281,9 +281,9 @@ class EstimationDecoderFC(nn.Module):
         x = F.elu(x)
         x = self.drop(x)
         x = self.fc3(x)
-        x = F.elu(x)
-        x = self.drop(x)
-        x = self.fc4(x)
+        # x = F.elu(x)
+        # x = self.drop(x)
+        # x = self.fc4(x)
         return torch.sigmoid(x)
 
 class ForceFC(nn.Module):
