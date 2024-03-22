6d063a7ef84e952b2befdaef1eeb597ea164797d
Mike Burgess <mjburgessjr@gmail.com>
2024-03-22 17:46:03 -0400

    No FW

--- train.py
+++ train.py
@@ -1146,7 +1146,7 @@ if __name__ == "__main__":
 
         # Logging on/off
         'use_wandb': True,
-        'run_name': 'FW_ELU_CNNrelu_L2Norm_NoTransforms_ExcludeTo200',
+        'run_name': 'NoFW_ELU_CNNrelu_L2Norm_NoTransforms_ExcludeTo200',
 
         # Training and model parameters
         'epochs'            : 60,
